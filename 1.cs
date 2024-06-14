ca981b85ab624b86b998ca968c8fbff6e3a6ede7
Mathzxl <162761845+Mathzxl@users.noreply.github.com>
2024-06-14 20:42:14 -0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class aluno
+    {
+        private string Nome;
+        private int matricula;
+        private double[] nota;
+        public double MédiaNotas(double[] notas)
+        {
+            double soma = 0;
+            for(int i = 0; i < 50; i++)
+            {
+                soma += nota[i];
+            }
+            double media = soma / 50;
+            return media;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Triângulo
+    {
+        public double lado1;
+        public double lado2;
+        public double lado3;
+        public Triângulo(double lado1, double lado2, double lado3)
+        {
+            this.lado1 = lado1;
+            this.lado2 = lado2;
+            this.lado3 = lado3;
+        }
+        public Triângulo()
+        {
+            this.lado1 = 1.4;
+            this.lado2 = 5.6;
+            this.lado3 = 5.4;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lado1,lado2,lado3;
+            Console.WriteLine("Dê o lado do triângulo");
+            lado1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dê o lado do triângulo");
+            lado2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dê o lado do triângulo");
+            lado3 = double.Parse(Console.ReadLine());
+            Triângulo triângulo = new Triângulo(lado1, lado2, lado3);
+            Console.WriteLine("\n" + triângulo.lado1);
+            Console.WriteLine(triângulo.lado2);
+            Console.WriteLine(triângulo.lado3);
+            Triângulo trg = new Triângulo();
+            Console.WriteLine("\n" + trg.lado1);
+            Console.WriteLine(trg.lado2);
+            Console.WriteLine(trg.lado3);
+            Console.ReadLine();
+        }
+    }
+}
